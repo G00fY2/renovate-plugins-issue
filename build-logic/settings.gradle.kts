3590057b9f5cb95b294400d6b3fa9e1777e1ebd3
Thomas Wirth <thomas-w-@web.de>
2026-01-11 00:16:26 +0100

    Use real world build-logic settings.gradle.kts

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,6 +1,27 @@
+rootProject.name = "build-logic"
+
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositories {
-        google()
+        google {
+            content {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        maven {
+            url = uri("https://developer.huawei.com/repo/")
+            content {
+                includeGroupAndSubgroups("com.huawei")
+            }
+        }
         mavenCentral()
+        gradlePluginPortal()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
     }
 }
